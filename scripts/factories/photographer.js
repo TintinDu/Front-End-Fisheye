986b57f5bef913283a035fd961c1e0d0476d3b30
martin.dubois <martin.dubois@foncia.com>
2023-05-11 13:10:59 +0200

    feat: update acessibility js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,21 +6,30 @@ function photographerFactory(data) {
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+    const link = document.createElement('a');
     const img = document.createElement('img');
+    link.setAttribute("href", `photographer.html`);
+    link.setAttribute("aria-label", name);
     img.setAttribute("src", picture);
+    img.setAttribute("alt", "");
+    img.className = "photographer__portrait";
     const h2 = document.createElement('h2');
-    h2.textContent = name;
-    const h3 = document.createElement('h3');
-    h3.textContent = city + ", " + country;
+    h2.textContent = name;``;
     const p1 = document.createElement('p');
-    p1.textContent = tagline;
+    p1.textContent = city + ", " + country;
+    p1.className = "photographer__location";
     const p2 = document.createElement('p');
-    p2.textContent = price + "€/jour";
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(h3);
+    p2.textContent = tagline;
+    p2.className = "photographer__tagline";
+    const p3 = document.createElement('p');
+    p3.textContent = price + "€/jour";
+    p3.className = "photographer__price";
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
     article.appendChild(p1);
     article.appendChild(p2);
+    article.appendChild(p3);
     return (article);
   }
   return { name, picture, getUserCardDOM };
