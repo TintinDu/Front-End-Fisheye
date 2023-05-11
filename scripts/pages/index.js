d4839c84cbab8b98199d8d5e6cdcf95b04e80d2c
martin.dubois <martin.dubois@foncia.com>
2023-05-11 16:28:46 +0200

    feat: manage nav between pages

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -13,7 +13,7 @@ async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
 
   photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
+    const photographerModel = photographerFactoryHomepage(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
     photographersSection.appendChild(userCardDOM);
   });
