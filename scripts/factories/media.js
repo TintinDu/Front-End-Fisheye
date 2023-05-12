abd91a49b2f2e24a7be64cfbadbb5f34f0a68b65
martin.dubois <martin.dubois@foncia.com>
2023-05-12 15:27:23 +0200

    feat: improve photographer style and dom

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -23,26 +23,44 @@ function mediaFactory(data) {
   const videos = medias.filter(filterOnlyVideos);
 
   const imgs = images.map((image) => {
+    const imageArticle = document.createElement('article');
+    const div = document.createElement('div');
     const imageName = image.image;
+    const imageTitle = image.title;
     const imagePath = `assets/images/${name}/${imageName}`;
     const img = document.createElement('img');
+    const title = document.createElement('h2');
+    title.textContent = imageTitle;
     img.setAttribute("src", imagePath);
     img.setAttribute("alt", image.title);
     img.className = "photographer__media";
-    return (img);
+    imageArticle.className = "media__container";
+    imageArticle.appendChild(img);
+    imageArticle.appendChild(div);
+    div.appendChild(title);
+    return (imageArticle);
   });
 
   const vids = videos.map((video) => {
+    const videoArticle = document.createElement('article');
+    const div = document.createElement('div');
     const videoName = video.video;
+    const videoTitle = video.title;
     const videoPath =`assets/images/${name}/${videoName}`;
     const vid = document.createElement('video');
+    const title = document.createElement('h2');
+    title.textContent = videoTitle;
     const source = document.createElement('source');
     source.setAttribute("src", videoPath);
     source.setAttribute("type", "video/mp4");
     vid.setAttribute("controls", "controls");
     vid.appendChild(source);
     vid.className = "photographer__media";
-    return (vid);
+    videoArticle.className = "media__container";
+    videoArticle.appendChild(vid);
+    videoArticle.appendChild(div);
+    div.appendChild(title);
+    return (videoArticle);
   });
 
   function getUserPhotographs() {
