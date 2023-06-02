12722c39c63b4c3fa6a33fdd8279ffddf2872eed
martin.dubois <martin.dubois@foncia.com>
2023-06-02 19:05:37 +0200

    feat: create likes dom

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -30,13 +30,20 @@ function mediaFactory(data) {
   const imageArticles = images.map((image) => {
     const imageArticle = document.createElement('article');
     const div = document.createElement('div');
+    const miniDiv = document.createElement('div');
     const imageName = image.image;
     const imageTitle = image.title;
     const imagePath = `assets/images/${name}/${imageName}`;
     const img = document.createElement('img');
     const title = document.createElement('h3');
+    const heart = document.createElement('img');
+    const likes = document.createElement('p');
+    likes.innerText = image.likes;
+    miniDiv.className = "media__likes";
+    heart.setAttribute("src", "assets/icons/heart.svg");
     title.textContent = imageTitle;
     title.className = "media__title";
+    div.className = "title__container";
     img.setAttribute("src", imagePath);
     img.setAttribute("alt", `open lightbox for ${image.title}`);
     img.setAttribute("id", image.id);
@@ -47,6 +54,9 @@ function mediaFactory(data) {
     imageArticle.appendChild(img);
     imageArticle.appendChild(div);
     div.appendChild(title);
+    div.appendChild(miniDiv);
+    miniDiv.appendChild(likes);
+    miniDiv.appendChild(heart);
     return (imageArticle);
   });
 
@@ -60,13 +70,20 @@ function mediaFactory(data) {
   const videoArticles = videos.map((video) => {
     const videoArticle = document.createElement('article');
     const div = document.createElement('div');
+    const miniDiv = document.createElement('div');
     const videoName = video.video;
     const videoTitle = video.title;
     const videoPath =`assets/images/${name}/${videoName}`;
     const vid = document.createElement('video');
     const title = document.createElement('h3');
+    const heart = document.createElement('img');
+    const likes = document.createElement('p');
+    likes.innerText = video.likes;
+    miniDiv.className = "likes";
+    heart.setAttribute("src", "assets/icons/heart.svg");
     title.textContent = videoTitle;
     title.className = "media__title";
+    div.className = "title__container";
     const source = document.createElement('source');
     source.setAttribute("src", videoPath);
     source.setAttribute("type", "video/mp4");
@@ -83,6 +100,9 @@ function mediaFactory(data) {
     videoArticle.appendChild(vid);
     videoArticle.appendChild(div);
     div.appendChild(title);
+    div.appendChild(miniDiv);
+    miniDiv.appendChild(likes);
+    miniDiv.appendChild(heart);
 
     return (videoArticle);
   });
